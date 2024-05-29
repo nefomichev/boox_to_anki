6b4151b17bd0b5b08e5ce626262caf36e70a9eed
Nikita Fomichev <nikita.fomichev@glovoapp.com>
2024-05-29 20:40:45 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,104 @@
+import re
+import os
+from dotenv import load_dotenv
+from dataclasses import dataclass
+import deepl
+from googletrans import Translator
+from googletrans.models import Translated
+from nltk.corpus import wordnet as wn
+
+load_dotenv()
+DEEPL_API_KEY = os.getenv('DEEPL_API_KEY')
+
+
+BOOX_TERM_DELIMITER = "--------------------------------------------------"
+
+@dataclass
+class Term:
+    term: str
+    translation_deepl: str
+    translation_google: str
+    definition: str
+    synonims: list[str]
+
+    def __str__(self):
+        return f'{self.term} | {self.translation_google}, {self.translation_deepl} | {self.definition}'
+
+    def __repr__(self):
+        return self.__str__()
+
+def parse_term(raw_terms: list[str]) -> set[str]:
+    pattern = r'^\s{3}(\w+)$' # 3 spaces and then a word
+    terms = []
+    for term in raw_terms:
+        matches = re.findall(pattern, term, re.MULTILINE)
+        if matches:
+            terms.extend((match.lower() for match in matches))
+    return set(terms)
+
+def tranlate_term_deepl(term: str) -> str:
+    if not DEEPL_API_KEY:
+        raise Exception('No DEEPL_API_KEY provided')
+    translator = deepl.Translator(DEEPL_API_KEY)
+    result = translator.translate_text(term, source_lang="EN",target_lang="RU")
+    if not isinstance(result, list):
+        result = [result]
+    translation = ', '.join([r.text for r in result])
+    return translation
+
+def translate_term_google(term: str) -> str:
+    translator = Translator()
+    translation = translator.translate(term, src='en', dest='ru')
+    if not isinstance(translation, Translated):
+        raise Exception('Translation failed for term:', term) 
+    return translation.text
+
+def get_definition(term: str) -> str:
+    results = []
+    synset = wn.synsets(term)[0]
+    if synset is not None:
+        results.append(synset.definition())
+    else:
+        results.append('No definition found')
+    return '\n'.join(results)
+
+def get_synonyms(term: str) -> list[str]:
+    synonyms = []
+    for synset in wn.synsets(term):
+        for lemma in synset.lemmas(): #type: ignore
+            synonyms.append(lemma.name())
+    return list(set(synonyms) - {term})[:3]
+
+def fill_term(term: str) -> Term:
+    translation_google = translate_term_google(term)
+    translation_deepl = tranlate_term_deepl(term)
+    definition = get_definition(term)
+    synonims = get_synonyms(term)
+    return Term(term, translation_deepl, translation_google, definition, synonims)
+    
+def reformat_vocabulary(input_file) -> list[Term]:
+    vocabulary: list[Term] = []
+    with open(input_file, 'r') as f:
+        content = f.read()
+    raw_terms: list[str] = content.split(BOOX_TERM_DELIMITER)
+    terms: set[str] = parse_term(raw_terms)
+    for term in terms:
+        vocabulary.append(fill_term(term))
+    return vocabulary
+
+def anki_card_generator(vocabulary: list[Term]) -> None:
+    with open('example_data/anki_cards.txt', 'w') as f:
+        for term in vocabulary:
+            f.write(f'{term.term}|({term.translation_deepl}, {term.translation_google}); {term.definition}; Synonims {term.synonims} \n')
+
+def main():
+    input_file = 'example_data/test.txt'
+    vocabulary = reformat_vocabulary(input_file)
+    anki_card_generator(vocabulary)
+    print('Anki cards generated successfully')
+    print('Check example_data/anki_cards.txt')
+    print(len(vocabulary), 'cards generated')
+    print('Vocabulary:', vocabulary)
+    
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Notes
+
+- Current solution is better than generic boox dictionary info
+- It's hard to understand to words without context or at least synonimns, I need to add better descriptions
+- Can I add some extra info the resuls is correct? Like the sentences or something like that 
